181691beeb2ade1c4323668e5f06e1e77dff62ec
Cobey King <cobeyking@live.com>
2021-10-08 15:53:41 -0500

    Finished media query for 500px, cant find the middle border?

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const { arrayExpression, arrayTypeAnnotation } = require("@babel/types");
+
 const artists = [
   {
     "id": 0,
@@ -284,9 +286,10 @@ Use addArtist to do the following:
 
 Example: addArtist(artists) should return the artists array with the above object added to the end of the array. */
 
-function addArtist(/*Your Code Here*/) {
-  /*Your Code Here*/
-}
+function addArtist(array, id, name, years, genre, nationality, bio) {
+  array.push({id, name, years, genre, nationality, bio});
+  return array;
+} 
 
 
 
@@ -297,9 +300,17 @@ Use lotsOfArt to do the following:
 
 For example lotsOfArt(artists); will return ["Amedeo Modigliani", "Rene Magritte", ... "Albrecht Dürer"]*/
 
-function lotsOfArt(/*Your Code Here*/) {
-  /*Your Code Here*/
+function lotsOfArt(array) {
+  const moreThan100 = [];
+  for(let i = 0;array.length > i; i++){
+    if array[i].paintings >= 100;
+    array.push(moreThan100);
+  }
+  
 }
+
+
+
 
 
 /* ***** END OF TASKS ***** */
